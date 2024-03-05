454c1fa7da5e418c67514bd82a803a81fce11c90
Snehalg1603 <160815111+Snehalg1603@users.noreply.github.com>
2024-03-05 14:45:43 +0530

    Java Basic Code

--- src/main/java/com/tch/student/repository/StudentRepository.java
+++ src/main/java/com/tch/student/repository/StudentRepository.java
@@ -1,4 +1,51 @@
 package com.tch.student.repository;
 
-public interface StudentRepository {
+import com.tch.student.dto.StudentDto;
+import com.tch.student.entity.StudentEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public interface StudentRepository   {
+    @Autowired
+    StudentRepository studentRepository;
+
+    @Autowired
+    AddressRepository addressRepository;
+
+    @Override
+    public StudentDto addStudent(StudentDto studentDto){
+        StudentEntity studentEntity = new StudentEntity();
+        studentEntity.setStudentName(studentDto.getStudentName());
+        studentEntity.setStudentDept(studentDto.getStudentDept());
+        studentEntity.setStudentDiv(studentDto.getStudentDiv());
+        studentEntity=studentRepository.save(studentEntity);
+        if(studentEntity!=null){
+            studentDto = new StudentDto();
+            studentDto.setStudentId(studentEntity.getStudentId());
+            studentDto.setStudentName(studentEntity.getStudentName());
+            studentDto.setStudentDept(studentEntity.getStudentDept());
+            studentDto.setStudentDiv(studentEntity.getStudentDiv());
+        }
+        return studentDto;
+
+    }
+    @Override
+    public List<StudentDto> getAllStudent(){
+        List<StudentEntity> studentEntities = studentRepository.findAll();
+        List<StudentDto>   studentDtos = new ArrayList<>();
+    }
+
+
 }
+
+
+
+
+
+
+
+
--- src/main/java/com/tch/student/service/StudentService.java
+++ src/main/java/com/tch/student/service/StudentService.java
@@ -1,4 +1,18 @@
 package com.tch.student.service;
 
-public class StudentService {
+import com.tch.student.dto.StudentDto;
+
+import java.util.List;
+
+public interface StudentService {
+
+    StudentDto addStudent(StudentDto studentDto);
+
+    StudentDto updateStudent(int studentId, StudentDto studentDto);
+
+    Boolean deleteStudent(int studentId);
+
+    StudentDto getStudent(int studentId);
+
+    List<StudentDto> getAllStudent();
 }
